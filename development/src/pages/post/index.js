3c51808b1275eaa9c055e4ff6b5d17fdab18d5a3
LIN HONG <mail.lin.hong@gmail.com>
2019-01-23 11:47:43 +0800

    增加文章内容页面样式

--- development/src/pages/post/index.js
+++ development/src/pages/post/index.js
@@ -9,6 +9,7 @@ import '../../element';
 import 'normalize.css';
 import 'element-ui/lib/theme-chalk/index.css';
 import '../../assets/stylesheets/reset.css';
+import '../../assets/stylesheets/post.css';
 
 Vue.prototype.$api = api;
 Vue.prototype.$config = config;
